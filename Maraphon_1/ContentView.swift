ce53a0a4f3253376894fa827db076bf607e3f0b0
Evgeny Evtushenko <enevtushenko@vtb.ru>
2023-12-01 18:28:44 +0500

    Scroll view task

---
+++ Maraphon_1/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Maraphon_1
+//
+//  Created by Evgeny Evtushenko on 01/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  let items: [Int] = Array(0..<100)
+  let height: CGFloat = 50
+  
+  var body: some View {
+    TabView {
+      ScrollView {
+        ScrollViewReader { reader in
+          ForEach(items, id: \.self) { item in
+            Text("\(item)")
+              .frame(maxWidth: .infinity)
+              .font(.title)
+              .foregroundStyle(.secondary)
+          }
+        }
+      }
+      .safeAreaInset(edge: .bottom, spacing: 50, content: {
+        Text("")
+      })
+      .overlay(alignment: .bottom) {
+        Color.red
+          .frame(height: height)
+          .opacity(0.8)
+      }
+      .tabItem {
+        Image(systemName: "star")
+        Text("Main")
+      }
+    }
+  }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Maraphon_1/Maraphon_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Maraphon_1App.swift
+//  Maraphon_1
+//
+//  Created by Evgeny Evtushenko on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Maraphon_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
